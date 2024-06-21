e2b18c73b61bb1f9d69dd5101151df404b6df437
Leo Danenkov <ledanenkov@gmail.com>
2024-06-20 22:29:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.135.0';
-import { VRButton } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/webxr/VRButton.js';
-import { XRHandModelFactory } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/webxr/XRHandModelFactory.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.135.0/build/three.module.js';
+import { VRButton } from 'https://cdn.jsdelivr.net/npm/three@0.135.0/examples/jsm/webxr/VRButton.js';
+import { XRHandModelFactory } from 'https://cdn.jsdelivr.net/npm/three@0.135.0/examples/jsm/webxr/XRHandModelFactory.js';
 
 const toneMappings = {
     piano: {
